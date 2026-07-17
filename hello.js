bb9d48f2da7b669b0be0314463c83f43bc352e8d
SupidoTR <supido.tr@gmail.com>
2026-07-18 00:26:04 +0600

    Тест 1

---
+++ hello.js
@@ -0,0 +1,11 @@
+const div = document.createElement("div");
+div.textContent = "Hello World";
+div.style.position = "fixed";
+div.style.top = "10px";
+div.style.right = "10px";
+div.style.padding = "10px";
+div.style.background = "red";
+div.style.color = "white";
+div.style.zIndex = "999999";
+
+document.body.appendChild(div);
